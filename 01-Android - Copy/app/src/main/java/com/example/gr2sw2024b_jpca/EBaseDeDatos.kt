75801836521e7f6d975d9caf2cbcf4c07e9cabbb
Jean Cotera <jean.cotera@gmail.com>
2025-02-10 19:23:11 -0500

    Proyecto

--- 01-Android - Copy/app/src/main/java/com/example/gr2sw2024b_jpca/EBaseDeDatos.kt
+++
@@ -1,8 +0,0 @@
-package com.example.gr2sw2024b_jpca
-
-class EBaseDeDatos {
-    companion object{
-        var tablaEmpresa: ESqliteHelperVehiculo? = null
-        var tablaProyecto: ESqliteHelperReparacion? = null
-    }
-}
